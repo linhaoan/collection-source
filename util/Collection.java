d7b575f0c62690a999a4af795965ba1975e3124b
linhao <330966654@qq.com>
2018-03-07 11:45:08 +0800

    collection 类解析

--- util/Collection.java
+++ util/Collection.java
@@ -9,6 +9,8 @@
   
 package source.java.util;
 
+import java.util.Iterator;
+
 /**
  * The root interface in the <i>collection hierarchy</i>.  A collection
  * represents a group of objects, known as its <i>elements</i>.  Some
@@ -106,7 +108,7 @@ package source.java.util;
  * implementor deems it appropriate.<br/>
  *
  * 集合框架接口中的许多方法都是用equals方法定义的。例如，包含（Object o）方法的规范说：
- * “如果这个集合包含至少一个元素e，那么返回true，这样（o==null？”e = = null:o.equals(e))。”该规范不应被理解为调用集合。
+ * “如果这个集合包含至少一个元素e，那么返回true，这样（o==null？”e == null:o.equals(e))。”该规范不应被理解为调用集合。
        包含一个非null的参数o将会为任何元素调用o.equals（e）。实现可以自由地实现对平等调用的优化，例如，首先比较这两个元素的散列码。
      （hashcode（）规范保证两个具有不相等的散列码的对象不能相等。）
         更一般地说，各种集合框架接口的实现可以自由地利用底层对象方法的指定行为，无论实现者认为它合适。
@@ -152,6 +154,181 @@ package source.java.util;
  * @since 1.2
  */
 public interface Collection<E> extends Iterable<E>{
-
+	
+	// Query Operations
+	// 查询操作
+    /**
+     * Returns the number of elements in this collection.  If this collection
+     * contains more than <tt>Integer.MAX_VALUE</tt> elements, returns
+     * <tt>Integer.MAX_VALUE</tt>.<br/>
+     * 返回这个集合中元素的数量。如果这个集合包含多于整数.MAX_VALUE元素,返回Integer.MAX_VALUE。
+     * @return the number of elements in this collection
+     */
+    int size();
+    
+    /**
+     * Returns <tt>true</tt> if this collection contains no elements.<br/>
+     * 如果这个集合不包含元素，那么返回true。
+     * @return <tt>true</tt> if this collection contains no elements
+     */
+    boolean isEmpty();
+    
+    /**
+     * Returns <tt>true</tt> if this collection contains the specified element.
+     * More formally, returns <tt>true</tt> if and only if this collection
+     * contains at least one element <tt>e</tt> such that
+     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.<br/>
+     * 如果该集合包含指定的元素，则返回true。更正式地说，如果这个集合包含至少一个元素e，那么返回true，这样（o==null？e = = null:o.equals(e))。
+     * @param o element whose presence in this collection is to be tested <br/>
+     * 在这个集合中存在的元素将被测试
+     * @return <tt>true</tt> if this collection contains the specified 
+     *         element <br/>
+     *         如果这个集合包含指定的元素，则返回true
+     * @throws ClassCastException if the type of the specified element
+     *         is incompatible with this collection
+     *         如果指定元素的类型与此集合不兼容
+     *         (<a href="#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *         collection does not permit null elements
+     *         如果指定的元素为null，这个集合不允许空元素
+     *         (<a href="#optional-restrictions">optional</a>)
+     */
+    boolean contains(Object o);
+    
+    /**
+     * Returns an iterator over the elements in this collection.  There are no
+     * guarantees concerning the order in which the elements are returned
+     * (unless this collection is an instance of some class that provides a
+     * guarantee).<br/>
+     * 在这个集合中返回一个迭代器。对于返回元素的顺序没有任何保证（除非这个集合是提供保证的类的实例）。
+     * 
+     * @return an <tt>Iterator</tt> over the elements in this collection
+     */
+    Iterator<E> iterator();
+    
+    /**
+     * Returns an array containing all of the elements in this collection.
+     * If this collection makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.<br/>
+     * 返回包含该集合中所有元素的数组。如果该集合对其迭代器返回的元素顺序有任何保证，那么该方法必须以相同的顺序返回元素。
+     * 
+     * <p>The returned array will be "safe" in that no references to it are
+     * maintained by this collection.  (In other words, this method must
+     * allocate a new array even if this collection is backed by an array).
+     * The caller is thus free to modify the returned array.<br/>
+     * 返回的数组将是“安全的”，因为这个集合没有对它进行任何引用。（换句话说，这个方法必须分配一个新的数组，即使这个集合是由数组支持的）。
+     * 因此，调用者可以自由地修改返回的数组。
+     * 
+     * <p>This method acts as bridge between array-based and collection-based
+     * APIs.<br/>
+     * 该方法充当基于数组的和基于集合的api之间的桥梁。
+     *
+     * @return an array containing all of the elements in this collection <br/>
+     * 包含该集合中的所有元素的数组
+     */
+    Object[] toArray();
+    
+    /**
+     * Returns an array containing all of the elements in this collection;
+     * the runtime type of the returned array is that of the specified array.
+     * If the collection fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this collection.<br/>
+     * 返回包含该集合中所有元素的数组;返回的数组的运行时类型是指定的数组。
+     * 如果集合适合于指定的数组，则返回该集合。否则，将使用指定数组的运行时类型和该集合的大小来分配一个新数组。
+     * 
+     * <p>If this collection fits in the specified array with room to spare
+     * (i.e., the array has more elements than this collection), the element
+     * in the array immediately following the end of the collection is set to
+     * <tt>null</tt>.  (This is useful in determining the length of this
+     * collection <i>only</i> if the caller knows that this collection does
+     * not contain any <tt>null</tt> elements.)<br/>
+     * 如果这个集合在指定的数组中有空闲空间（即：这个数组比这个集合有更多的元素），在集合结束后的数组中的元素被设置为null。
+     * （只有在调用者知道该集合不包含任何空元素时，才能确定该集合的长度。）
+     *
+     * <p>If this collection makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.<br/>
+     * 如果该集合对其迭代器返回的元素顺序有任何保证，那么该方法必须以相同的顺序返回元素。
+     *
+     * <p>Like the {@link #toArray()} method, this method acts as bridge between
+     * array-based and collection-based APIs.  Further, this method allows
+     * precise control over the runtime type of the output array, and may,
+     * under certain circumstances, be used to save allocation costs.<br/>
+     * 与toArray（）方法一样，该方法充当基于数组的和基于集合的api之间的桥梁。
+     * 此外，该方法允许对输出数组的运行时类型进行精确控制，并且在某些情况下可以使用它来节省分配成本。
+     *
+     * <p>Suppose <tt>x</tt> is a collection known to contain only strings.
+     * The following code can be used to dump the collection into a newly
+     * allocated array of <tt>String</tt>:
+     *
+     * <pre>
+     *     String[] y = x.toArray(new String[0]);</pre>
+     *
+     * Note that <tt>toArray(new Object[0])</tt> is identical in function to
+     * <tt>toArray()</tt>.<br/>
+     * 
+     * 假设x是只包含字符串的集合。下面的代码可用于将集合转储为新分配的字符串数组：
+     * 
+     *    String[] y = x.toArray(new String[0]);
+     * 
+     * 注意，toArray(new Object[0])在函数中与toArray()是相同的。
+     *
+     * @param <T> the runtime type of the array to contain the collection
+     * 包含该集合的数组的运行时类型
+     * @param a the array into which the elements of this collection are to be
+     *        stored, if it is big enough; otherwise, a new array of the same
+     *        runtime type is allocated for this purpose.<br/>
+     *        如果数据足够大，则存储该集合元素的数组;否则，将为这个目的分配相同的运行时类型的新数组。
+     * @return an array containing all of the elements in this collection
+     * 		     包含该集合中的所有元素的数组
+     * @throws ArrayStoreException if the runtime type of the specified array
+     *         is not a supertype of the runtime type of every element in
+     *         this collection
+     *         如果指定数组的运行时类型不是该集合中每个元素的运行时类型的超类型
+     * @throws NullPointerException if the specified array is null
+     * 		       如果指定的数组为空
+     */
+    <T> T[] toArray(T[] a);
+    
+    
+    // Modification Operations
+    // 修改操作
+    
+    /**
+     * Ensures that this collection contains the specified element (optional
+     * operation).  Returns <tt>true</tt> if this collection changed as a
+     * result of the call.  (Returns <tt>false</tt> if this collection does
+     * not permit duplicates and already contains the specified element.)<p>
+     *
+     * Collections that support this operation may place limitations on what
+     * elements may be added to this collection.  In particular, some
+     * collections will refuse to add <tt>null</tt> elements, and others will
+     * impose restrictions on the type of elements that may be added.
+     * Collection classes should clearly specify in their documentation any
+     * restrictions on what elements may be added.<p>
+     *
+     * If a collection refuses to add a particular element for any reason
+     * other than that it already contains the element, it <i>must</i> throw
+     * an exception (rather than returning <tt>false</tt>).  This preserves
+     * the invariant that a collection always contains the specified element
+     * after this call returns.
+     *
+     * @param e element whose presence in this collection is to be ensured
+     * @return <tt>true</tt> if this collection changed as a result of the
+     *         call
+     * @throws UnsupportedOperationException if the <tt>add</tt> operation
+     *         is not supported by this collection
+     * @throws ClassCastException if the class of the specified element
+     *         prevents it from being added to this collection
+     * @throws NullPointerException if the specified element is null and this
+     *         collection does not permit null elements
+     * @throws IllegalArgumentException if some property of the element
+     *         prevents it from being added to this collection
+     * @throws IllegalStateException if the element cannot be added at this
+     *         time due to insertion restrictions
+     */
+    boolean add(E e);
 }
   
