6c4ce6c44759680c00c485784ce7061423d88dfa
linhao <330966654@qq.com>
2018-03-09 17:15:33 +0800

    抽象List 源代码

--- util/AbstractList.java
+++ util/AbstractList.java
@@ -11,8 +11,11 @@ package source.java.util;
 
 import java.util.AbstractSequentialList;
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  * This class provides a skeletal implementation of the {@link List}
@@ -235,20 +238,189 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
     /**
      * Removes all of the elements from this list (optional operation).
      * The list will be empty after this call returns.
-     *
+     * 从这个列表删除的所有元素(可选操作)。这个调用返回后的列表是空的。
      * <p>This implementation calls {@code removeRange(0, size())}.
-     *
+     * 这个实现调用removeRange(0,size())。
      * <p>Note that this implementation throws an
      * {@code UnsupportedOperationException} unless {@code remove(int
      * index)} or {@code removeRange(int fromIndex, int toIndex)} is
      * overridden.
-     *
+     * 请注意,此实现抛出UnsupportedOperationException
+     * 除非方式remove(int index)或removeRange(int fromIndex,int toIndex)方法被覆盖。
      * @throws UnsupportedOperationException if the {@code clear} operation
      *         is not supported by this list
      */
     public void clear() {
         removeRange(0, size());
     }
+    
+    /**
+     * {@inheritDoc}
+     *
+     * <p>This implementation gets an iterator over the specified collection
+     * and iterates over it, inserting the elements obtained from the
+     * iterator into this list at the appropriate position, one at a time,
+     * using {@code add(int, E)}.
+     * Many implementations will override this method for efficiency.
+     * 
+     * 这个实现在指定的集合上得到一个迭代器，并在它上迭代，将从迭代器中获得的元素插入到适当位置的列表中，一次使用add（int，E），
+     * 许多实现将重写这个方法以提高效率。
+     * <p>Note that this implementation throws an
+     * {@code UnsupportedOperationException} unless
+     * {@link #add(int, Object) add(int, E)} is overridden.
+     * 请注意,此实现抛出UnsupportedOperationException除非方法add(int,E)覆盖。
+     * @throws UnsupportedOperationException {@inheritDoc}
+     * @throws ClassCastException            {@inheritDoc}
+     * @throws NullPointerException          {@inheritDoc}
+     * @throws IllegalArgumentException      {@inheritDoc}
+     * @throws IndexOutOfBoundsException     {@inheritDoc}
+     */
+    public boolean addAll(int index, Collection<? extends E> c) {
+        rangeCheckForAdd(index);
+        boolean modified = false;
+        for (E e : c) {
+            add(index++, e);
+            modified = true;
+        }
+        return modified;
+    }
+    
+    // Iterators
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     *
+     * <p>This implementation returns a straightforward implementation of the
+     * iterator interface, relying on the backing list's {@code size()},
+     * {@code get(int)}, and {@code remove(int)} methods.
+     *
+     * <p>Note that the iterator returned by this method will throw an
+     * {@link UnsupportedOperationException} in response to its
+     * {@code remove} method unless the list's {@code remove(int)} method is
+     * overridden.
+     *
+     * <p>This implementation can be made to throw runtime exceptions in the
+     * face of concurrent modification, as described in the specification
+     * for the (protected) {@link #modCount} field.
+     *
+     * @return an iterator over the elements in this list in proper sequence
+     */
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+    
+    private class Itr implements Iterator<E> {
+    	 
+        /**
+         * Index of element to be returned by subsequent call to next.
+         * 索引的元素返回的后续调用。
+         */
+		int cursor = 0;
+		
+        /**
+         * Index of element returned by most recent call to next or
+         * previous.  Reset to -1 if this element is deleted by a call
+         * to remove.
+         * 最近或以前的调用返回的元素的索引。如果这个元素被删除的调用删除，则重置为-1。
+         * 1 初始化时为-1 。
+         * 2 删除元素后为 -1。
+         */
+        int lastRet = -1;
+        
+        int expectedModCount = modCount;
+       
+		@Override
+		public boolean hasNext() {			
+			return cursor!=size();
+		}
+
+		@Override
+		public E next() {
+			checkForComodification();
+			try {
+				int i = cursor;
+				E e = get(i);
+				lastRet = i;
+				cursor = i + 1;
+				return e;
+			} catch (IndexOutOfBoundsException e) {
+				checkForComodification();
+				throw new NoSuchElementException();
+			}
+
+		}
+		
+		public void remove() {
+			if (lastRet < 0) {
+				throw new IllegalStateException();
+			}
+			checkForComodification();
+			try {				
+				AbstractList.this.remove(lastRet);
+				if (lastRet < cursor) {
+					cursor--;
+				}
+				lastRet = -1;
+				expectedModCount = modCount;
+			} catch (IndexOutOfBoundsException e) {
+				checkForComodification();
+				throw new ConcurrentModificationException();
+			}
+		}
+		
+        final void checkForComodification() {
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+        }
+    	
+    }
+    
+    private class ListItr extends Itr implements ListIterator<E> {
+
+		@Override
+		public boolean hasPrevious() {
+			  
+			//  Auto-generated method stub  
+			return false;
+		}
+
+		@Override
+		public E previous() {
+			  
+			//  Auto-generated method stub  
+			return null;
+		}
+
+		@Override
+		public int nextIndex() {
+			  
+			//  Auto-generated method stub  
+			return 0;
+		}
+
+		@Override
+		public int previousIndex() {
+			  
+			//  Auto-generated method stub  
+			return 0;
+		}
+
+		@Override
+		public void set(E e) {
+			  
+			//  Auto-generated method stub  
+			
+		}
+
+		@Override
+		public void add(E e) {
+			  
+			//  Auto-generated method stub  
+			
+		}
+    	
+    }
+    
     
     
   // Comparison and hashing
@@ -259,19 +431,26 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
      * Shifts any succeeding elements to the left (reduces their index).
      * This call shortens the list by {@code (toIndex - fromIndex)} elements.
      * (If {@code toIndex==fromIndex}, this operation has no effect.)
-     *
+     * 从这个列表中删除所有索引在fromIndex、包含和toIndex之间的元素。
+     * 将任何成功的元素转移到左边（减少它们的索引）。
+     * 这个调用缩短了列表（toIndex-fromIndex）元素。（如果toIndex==fromIndex，该操作没有效果。）
+     * 
      * <p>This method is called by the {@code clear} operation on this list
      * and its subLists.  Overriding this method to take advantage of
      * the internals of the list implementation can <i>substantially</i>
      * improve the performance of the {@code clear} operation on this list
      * and its subLists.
-     *
+     * 这个方法是通过这个列表及其子列表的清晰操作来调用的。
+     * 重写这个方法以利用列表实现的内部机制可以极大地改善这个列表及其子列表中的clear操作的性能。
+     * 
      * <p>This implementation gets a list iterator positioned before
      * {@code fromIndex}, and repeatedly calls {@code ListIterator.next}
      * followed by {@code ListIterator.remove} until the entire range has
      * been removed.  <b>Note: if {@code ListIterator.remove} requires linear
      * time, this implementation requires quadratic time.</b>
-     *
+     * 这个实现得到一个list迭代器在fromIndex之前，并重复调用ListIterator。
+     * 下一个ListIterator紧随其后。移除直到整个范围被移除。
+     * 
      * @param fromIndex index of first element to be removed
      * @param toIndex index after last element to be removed
      */
@@ -281,6 +460,51 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
             it.next();
             it.remove();
         }
+    }
+    
+    /**
+     * The number of times this list has been <i>structurally modified</i>.
+     * Structural modifications are those that change the size of the
+     * list, or otherwise perturb it in such a fashion that iterations in
+     * progress may yield incorrect results.
+     * 这个列表的次数是经过结构修改的。结构修改是改变列表的大小，或者以这样一种方式扰乱它，使迭代中的迭代可能产生不正确的结果。
+     * 
+     * <p>This field is used by the iterator and list iterator implementation
+     * returned by the {@code iterator} and {@code listIterator} methods.
+     * If the value of this field changes unexpectedly, the iterator (or list
+     * iterator) will throw a {@code ConcurrentModificationException} in
+     * response to the {@code next}, {@code remove}, {@code previous},
+     * {@code set} or {@code add} operations.  This provides
+     * <i>fail-fast</i> behavior, rather than non-deterministic behavior in
+     * the face of concurrent modification during iteration.
+     * 这个字段由迭代器和list迭代器实现，迭代器和listIterator方法返回。
+     * 如果这个字段的值变化出乎意料,迭代器(或列表迭代器)将抛出ConcurrentModificationException以应对下一个,删除之前设置或添加操作。
+     * 这提供了故障快速的行为，而不是在迭代过程中进行并发修改时的非确定性行为。
+     *
+     * <p><b>Use of this field by subclasses is optional.</b> If a subclass
+     * wishes to provide fail-fast iterators (and list iterators), then it
+     * merely has to increment this field in its {@code add(int, E)} and
+     * {@code remove(int)} methods (and any other methods that it overrides
+     * that result in structural modifications to the list).  A single call to
+     * {@code add(int, E)} or {@code remove(int)} must add no more than
+     * one to this field, or the iterators (and list iterators) will throw
+     * bogus {@code ConcurrentModificationExceptions}.  If an implementation
+     * does not wish to provide fail-fast iterators, this field may be
+     * ignored.
+     * 在子类中使用这个字段是可选的。如果一个子类希望提供故障快速迭代器（和列表迭代器），
+     * 那么它只需要在add（int、E）和remove（int）方法中增加这个字段（以及它覆盖对该列表进行结构修改的任何其他方法）。
+     * 一个调用add(int,E)或remove(int)必须不超过一个添加到这个领域,或者迭代器(和列表迭代器)将把虚假concurrentmodificationexceptions。
+     * 如果一个实现不希望提供故障快速迭代器，那么这个字段可能会被忽略。
+     */
+    protected transient int modCount = 0;
+    
+    private void rangeCheckForAdd(int index) {
+        if (index < 0 || index > size())
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+    
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size();
     }
 }
   
