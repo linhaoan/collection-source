8590629b191f3809fd18a91bec030bc9781b6660
linhaoan <330966654@qq.com>
2018-03-16 21:44:38 +0800

    源码

--- java/util/HashMap.java
+++ java/util/HashMap.java
@@ -21,6 +21,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.HashMap.Node;
+import java.util.HashMap.TreeNode;
 
 /**
  * Hash table based implementation of the <tt>Map</tt> interface.  This
@@ -493,5 +494,267 @@ public class HashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>,Cloneable
      * @serial
      */
     final float loadFactor;
+    
+    /**
+     * Constructs an empty <tt>HashMap</tt> with the specified initial
+     * capacity and load factor.
+     * 构造具有指定初始容量和负载因子的空HashMap。
+     * @param  initialCapacity the initial capacity
+     * @param  loadFactor      the load factor
+     * @throws IllegalArgumentException if the initial capacity is negative
+     *         or the load factor is nonpositive
+     */
+    public HashMap(int initialCapacity, float loadFactor) {
+    	if (initialCapacity<0) {
+    		 throw new IllegalArgumentException("Illegal initial capacity: " +
+                     initialCapacity);
+    	}
+    	if (initialCapacity > MAXIMUM_CAPACITY) {
+    		initialCapacity = MAXIMUM_CAPACITY;
+    	}
+    	if (loadFactor < 0 || Float.isNaN(loadFactor)) {
+    		  throw new IllegalArgumentException("Illegal load factor: " +
+                      loadFactor);
+    	}
+        this.loadFactor = loadFactor;
+        this.threshold = tableSizeFor(initialCapacity);
+    }
+    
+    /**
+     * Constructs an empty <tt>HashMap</tt> with the specified initial
+     * capacity and the default load factor (0.75).
+     * 构造具有指定初始容量和默认加载因子(0.75)的空HashMap。
+     * 
+     * @param  initialCapacity the initial capacity.
+     * @throws IllegalArgumentException if the initial capacity is negative.
+     */
+    public HashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+    
+    /**
+     * Constructs an empty <tt>HashMap</tt> with the default initial capacity
+     * (16) and the default load factor (0.75).
+     * 构造一个带有默认初始容量(16)和默认负载因子(0.75)的空HashMap。
+     */
+    public HashMap() {
+        this.loadFactor = DEFAULT_LOAD_FACTOR; // all other fields defaulted
+    }
+    
+    /**
+     * Constructs a new <tt>HashMap</tt> with the same mappings as the
+     * specified <tt>Map</tt>.  The <tt>HashMap</tt> is created with
+     * default load factor (0.75) and an initial capacity sufficient to
+     * hold the mappings in the specified <tt>Map</tt>.
+     * 使用与指定映射相同的映射构造新的HashMap。
+     * HashMap使用默认加载因子(0.75)创建，初始容量足以在指定映射中保存映射。
+     * 
+     * @param   m the map whose mappings are to be placed in this map
+     * @throws  NullPointerException if the specified map is null
+     */
+    public HashMap(Map<? extends K, ? extends V> m) {
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+        putMapEntries(m, false);
+    }
+    
+    /**
+     * Implements Map.putAll and Map constructor
+     *
+     * @param m the map
+     * @param evict false when initially constructing this map, else
+     * true (relayed to method afterNodeInsertion).
+     */
+    final void putMapEntries(Map<? extends K, ? extends V> m, boolean evict) {
+        int s = m.size();
+        if (s > 0) {
+            if (table == null) { // pre-size
+                float ft = ((float)s / loadFactor) + 1.0F;
+                int t = ((ft < (float)MAXIMUM_CAPACITY) ?
+                         (int)ft : MAXIMUM_CAPACITY);
+                if (t > threshold)
+                    threshold = tableSizeFor(t);
+            } else if (s > threshold) {
+            	resize();
+            }
+                
+            for (Map.Entry<? extends K, ? extends V> e : m.entrySet()) {
+                K key = e.getKey();
+                V value = e.getValue();
+                putVal(hash(key), key, value, false, evict);
+            }
+        }
+    }
+    
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key, the old
+     * value is replaced.
+     * 将指定的值与此映射中的指定键关联。
+     * 如果映射之前包含了键的映射，那么旧的值将被替换。
+     * 
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with <tt>key</tt>, or
+     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     *         (A <tt>null</tt> return can also indicate that the map
+     *         previously associated <tt>null</tt> with <tt>key</tt>.)
+     */
+    public V put(K key, V value) {
+        return putVal(hash(key), key, value, false, true);
+    }
+    
+    /**
+     * Implements Map.put and related methods
+     *
+     * @param hash hash for key
+     * @param key the key
+     * @param value the value to put
+     * @param onlyIfAbsent if true, don't change existing value
+     * 如果是真的，就不要改变现有的价值。
+     * @param evict if false, the table is in creation mode.
+     * 如果错误被逐出，表处于创建模式。
+     * @return previous value, or null if none
+     */
+    final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
+                   boolean evict) {
+        Node<K,V>[] tab;
+        Node<K,V> p; 
+        int n, i;
+        // 步骤①：tab为空则创建
+        if ((tab = table) == null || (n = tab.length) == 0) {
+        	 n = (tab = resize()).length;
+        }          
+        if ((p = tab[i = (n - 1) & hash]) == null) {
+        	//tab[i] 为null ,直接插入
+        	tab[i] = newNode(hash, key, value, null);
+        }  else {
+            Node<K,V> e; 
+            K k;
+            if (p.hash == hash && 
+            		((k = p.key) == key || (key != null && key.equals(k)))) {
+            	//key 存在，直接插入
+            	e = p;
+            } else if (p instanceof TreeNode) {
+            	//key 不存在，并且是树结点,直接插入键值对
+            	e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
+            } else {
+            	// key 不存在，并且是链表结点
+                for (int binCount = 0; ; ++binCount) {//开始遍历结点，准备插入                	
+                    if ((e = p.next) == null) {
+                        p.next = newNode(hash, key, value, null);
+                        if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+                        	//链表长度大于8，准备转入红黑树插入
+                            treeifyBin(tab, hash);
+                        break;
+                    }
+                    if (e.hash == hash &&
+                        ((k = e.key) == key || (key != null && key.equals(k))))
+                        break;
+                    //链表插入，若key存在，直接覆盖value
+                    p = e;
+                }
+            }
+            if (e != null) { // existing mapping for key
+                V oldValue = e.value;
+                if (!onlyIfAbsent || oldValue == null)
+                    e.value = value;
+                afterNodeAccess(e);
+                return oldValue;
+            }
+        }
+        ++modCount;
+        // 步骤⑥：超过最大容量 就扩容
+        if (++size > threshold) {
+        	  resize();
+        }         
+        afterNodeInsertion(evict);
+        return null;
+    }
+    
+    /**
+     * Initializes or doubles table size.  If null, allocates in
+     * accord with initial capacity target held in field threshold.
+     * Otherwise, because we are using power-of-two expansion, the
+     * elements from each bin must either stay at same index, or move
+     * with a power of two offset in the new table.
+     * 初始化或折叠表大小。
+     * 如果为空，则按照字段阈值中的初始容量目标分配。
+     * 否则，因为我们使用的是2的幂拓展，所以每个bin的元素必须保持相同的索引，
+     * 或者在新表中使用2的幂偏移量。
+     * @return the table
+     */
+    final Node<K,V>[] resize() {
+        Node<K,V>[] oldTab = table;
+        int oldCap = (oldTab == null) ? 0 : oldTab.length;
+        int oldThr = threshold;
+        int newCap, newThr = 0;
+        if (oldCap > 0) {
+            if (oldCap >= MAXIMUM_CAPACITY) {
+                threshold = Integer.MAX_VALUE;
+                return oldTab;
+            }
+            else if ((newCap = oldCap << 1) < MAXIMUM_CAPACITY &&
+                     oldCap >= DEFAULT_INITIAL_CAPACITY)
+                newThr = oldThr << 1; // double threshold
+        }
+        else if (oldThr > 0) // initial capacity was placed in threshold
+            newCap = oldThr;
+        else {               // zero initial threshold signifies using defaults
+            newCap = DEFAULT_INITIAL_CAPACITY;
+            newThr = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+        }
+        if (newThr == 0) {
+            float ft = (float)newCap * loadFactor;
+            newThr = (newCap < MAXIMUM_CAPACITY && ft < (float)MAXIMUM_CAPACITY ?
+                      (int)ft : Integer.MAX_VALUE);
+        }
+        threshold = newThr;
+        @SuppressWarnings({"rawtypes","unchecked"})
+            Node<K,V>[] newTab = (Node<K,V>[])new Node[newCap];
+        table = newTab;
+        if (oldTab != null) {
+            for (int j = 0; j < oldCap; ++j) {
+                Node<K,V> e;
+                if ((e = oldTab[j]) != null) {
+                    oldTab[j] = null;
+                    if (e.next == null)
+                        newTab[e.hash & (newCap - 1)] = e;
+                    else if (e instanceof TreeNode)
+                        ((TreeNode<K,V>)e).split(this, newTab, j, oldCap);
+                    else { // preserve order
+                        Node<K,V> loHead = null, loTail = null;
+                        Node<K,V> hiHead = null, hiTail = null;
+                        Node<K,V> next;
+                        do {
+                            next = e.next;
+                            if ((e.hash & oldCap) == 0) {
+                                if (loTail == null)
+                                    loHead = e;
+                                else
+                                    loTail.next = e;
+                                loTail = e;
+                            }
+                            else {
+                                if (hiTail == null)
+                                    hiHead = e;
+                                else
+                                    hiTail.next = e;
+                                hiTail = e;
+                            }
+                        } while ((e = next) != null);
+                        if (loTail != null) {
+                            loTail.next = null;
+                            newTab[j] = loHead;
+                        }
+                        if (hiTail != null) {
+                            hiTail.next = null;
+                            newTab[j + oldCap] = hiHead;
+                        }
+                    }
+                }
+            }
+        }
+        return newTab;
+    }
 }
   
